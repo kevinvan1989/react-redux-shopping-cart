c51adb1dee374ea22f3ac27ffca267ff72d238b6
Kevin Vanantwerpen <>
2020-10-15 23:10:01 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App grid-container">
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+      <main>
+        main
+      </main>
+      <footer>
+        foot
+      </footer>
     </div>
   );
 }
